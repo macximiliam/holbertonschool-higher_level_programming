115103ce937e3f6dcb1a1ec60185d51b1e595794
macximiliam <alexanderbigman29@gmail.com>
2025-09-15 02:32:38 +0000

    Add a program that import functions

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, add(a, b)))
+    print("{} / {} = {}".format(a, b, add(a, b)))
+    print("{} * {} = {}".format(a, b, add(a, b)))
