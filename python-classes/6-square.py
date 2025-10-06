99c83bd56b9f9162a97d4ddbc507c8732afa3fb6
macxiliam <alexanderbiman@gmail.com>
2025-10-05 20:48:49 -0500

    Add a project of class and poo in python

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -34,7 +34,7 @@ class Square:
     def position(self, value):
         """Set the position of the square."""
         if not isinstance(value, tuple):
-            raise TypeError("positon must be a tuple of 2 positive integers")
+            raise TypeError("position must be a tuple of 2 positive integers")
         if len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         if not all(isinstance(num, int) and num >= 0 for num in value):
