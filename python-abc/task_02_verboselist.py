db86afbc9dbe7329ca13e5bf1a7feb4c6727ac63
macxiliam <alexanderbiman@gmail.com>
2025-10-26 20:23:39 -0500

    Add projct Abtract Class, Interface, Subclassing

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
+"""Defines a class VerboseList that prints messages on list actions."""
 
 
 class VerboseList(list):
+    """List that shows messages when modified."""
 
     def append(self, item):
+        """Add item to list and show message."""
         super().append(item)
         print(f"Added [{item}] to the list.")
 
     def extend(self, iterable):
+        """Add several items and show message."""
         super().extend(iterable)
-        print(f"Extend the list whith [{len (iterable)}].")
+        print(f"Extend the list with [{len(iterable)}].")
 
     def remove(self, item):
+        """Remove item and show message."""
         super().remove(item)
         print(f"Remove [{item}] from the list.")
 
-
     def pop(self, index=-1):
-        item = self[index]
+        """Remove item by index and show message."""
+        item = super().pop(index)
         print(f"Popped [{item}] from the list.")
- 
