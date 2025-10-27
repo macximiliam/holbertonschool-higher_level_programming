7f556a3a7e46ded96254ddab66dd6ff5e503d867
macxiliam <alexanderbiman@gmail.com>
2025-10-26 20:18:02 -0500

    Add projct Abtract Class, Interface, Subclassing

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,3 +1,22 @@
 #!/usr/bin/python3
-from abc import ABC, abstractmethod 
 
+
+class VerboseList(list):
+
+    def append(self, item):
+        super().append(item)
+        print(f"Added [{item}] to the list.")
+
+    def extend(self, iterable):
+        super().extend(iterable)
+        print(f"Extend the list whith [{len (iterable)}].")
+
+    def remove(self, item):
+        super().remove(item)
+        print(f"Remove [{item}] from the list.")
+
+
+    def pop(self, index=-1):
+        item = self[index]
+        print(f"Popped [{item}] from the list.")
+ 
