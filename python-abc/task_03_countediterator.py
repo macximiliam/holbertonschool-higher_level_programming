7525a8e82de84afef8086b7c83d6de82d6025b4d
macxiliam <alexanderbiman@gmail.com>
2025-10-26 22:19:31 -0500

    Add projct Abtract Class, Interface, Subclassing

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 class CountedIterator:
-    pass
 
     def __init__(self, iterable):
         self.iterator = iter(iterable)
@@ -10,10 +9,10 @@ class CountedIterator:
     def __next__(self):
         item = next(self.iterator)
         self.count += 1
+        return item
 
     def get_count(self):
         return self.count
     
-        
-
-
+    def __iter__(self):
+        return self
