8d5104064792a7f5eea8000f1e6df0a84f722710
macxiliam <alexanderbiman@gmail.com>
2025-10-26 22:13:03 -0500

    Add projct Abtract Class, Interface, Subclassing

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
+"""
+This module demonstrates multiple inheritance using mixins.
+It includes SwimMixin, FlyMixin, and Dragon classes.
+"""
 
 class SwimMixin:
+    """Mixin that provides swimming behavior."""
+    
     def swim(self):
-        print("he creature swims!")
+        """Prints a message showing that the creature can swim."""
+        print("The creature swims!")
 
 
 class FlyMixin:
+    """Mixin that provides flying behavior."""
+    
     def fly(self):
+        """Prints a message showing that the creature can fly."""
         print("The creature flies!")
 
 
 class Dragon(SwimMixin, FlyMixin):
+    """Represents a dragon that can both swim and fly."""
+    
     def roar(self):
+        """Prints a message showing that the dragon can roar."""
         print("The dragon roars!")
