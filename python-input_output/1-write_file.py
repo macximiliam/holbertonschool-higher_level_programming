b7546ede40436b54a9554c261b2f0567efce9945
macxiliam <alexanderbiman@gmail.com>
2025-11-02 15:52:06 -0500

    Add the project baut input_output wiht python

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,6 +5,6 @@
 
 def write_file(filename="", text=""):
     """Write a string to a text file (UTF8) and return chars written"""
-    
+
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
