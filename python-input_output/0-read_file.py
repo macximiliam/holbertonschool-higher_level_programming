1839f57e6d11c74082be88a824828b1e808d8123
macxiliam <alexanderbiman@gmail.com>
2025-11-02 15:41:03 -0500

    Add the project baut input_output wiht python

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module that defines a function to read a text file and print it to stdout.
+"""Module  that defines a function to read a text file and print it to stdout.
 """
 
 
