5e0d9ddfc8c04d2923afc928c87b86bed108276a
macxiliam <alexanderbiman@gmail.com>
2025-11-02 15:34:27 -0500

    Add the project baut input_output wiht python

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,9 +2,7 @@
 """Module that defines a function to read a text file (UTF8) and print it to stdout.
 """
 
-
 def read_file(filename=""):
     """Read a text file (UTF8) and print its content to stdout"""
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
-
