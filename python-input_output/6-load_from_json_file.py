5eb550c84a0aa73142ad35fd38800374ba2d5c18
macxiliam <alexanderbiman@gmail.com>
2025-11-05 20:31:50 -0500

    Add a script that adds all arguments to a Python list, and then save them to a file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module for loading Python objects from JSON files"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """Return the Python object stored in a JSON file"""
+    with open(filename, encoding="utf-8") as f:
+        return json.load(f)
+    
---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module to convert class objects to JSON-serializable dict
+"""
+
+
+def class_to_json(obj):
+    return {key: value for key, value in vars(obj).items()
+            if isinstance(value,(list, dict, str, int, bool))}
