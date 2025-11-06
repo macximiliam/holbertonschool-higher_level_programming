5582f1d0346d29ced0c3384141bf398635ac6af9
macxiliam <alexanderbiman@gmail.com>
2025-11-05 20:41:57 -0500

    Add a script that adds all arguments to a Python list, and then save them to a file

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -5,4 +5,4 @@
 
 def class_to_json(obj):
     return {key: value for key, value in vars(obj).items()
-            if isinstance(value,(list, dict, str, int, bool))}
+            if isinstance(value, (list, dict, str, int, bool))}
