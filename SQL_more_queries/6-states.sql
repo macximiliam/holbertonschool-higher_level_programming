629d6c19152b939955b6081bfd9c2f1395f1fcf6
macxiliam <alexanderbiman@gmail.com>
2026-01-18 18:31:58 -0500

    Add a a script that lists all the cities of California that can be found in the database hbtn_0d_usa

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,10 +1,6 @@
--- Creates the database hbtn_0d_usa if it does not exist
+-- Creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
--- Selects the database hbtn_0d_usa
 USE hbtn_0d_usa;
-
--- Creates the table states if it does not exist
 CREATE TABLE IF NOT EXISTS states (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,11 +1,6 @@
--- Crea la base de datos si no existe ya
+-- Script that creates the database hbtn_0d_usa and the table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
--- Selecciona la base de datos para usarla
 USE hbtn_0d_usa;
-
--- Crea la tabla 'cities' si no existe ya
--- state_id es una llave foránea que referencia a states(id)
 CREATE TABLE IF NOT EXISTS cities (
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     state_id INT NOT NULL,
