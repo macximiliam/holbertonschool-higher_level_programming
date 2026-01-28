58d12c785d5f8ce7b73d11a963588e6f2b85208e
macxiliam <alexanderbiman@gmail.com>
2026-01-27 23:22:14 -0500

    Add a script that lists all states from the database hbtn_0e_0_usa

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,31 +1,32 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-List all states from the database hbtn_0e_0_usa.
+This script lists all states from the database hbtn_0e_0_usa.
+The script takes 3 arguments: mysql username, mysql password and database name.
 """
-
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
+    # Connects to the database using the arguments provided in the command line.
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=username,
-        passwd=password,
-        db=database
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
 
+    # Creates a cursor object to execute SQL queries.
     cursor = db.cursor()
 
+    # Executes the query to fetch all states ordered by id.
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    for row in cursor.fetchall():
+    # Fetches all rows and prints them in the required format.
+    rows = cursor.fetchall()
+    for row in rows:
         print(row)
 
+    # Closes the cursor and the database connection.
     cursor.close()
     db.close()
