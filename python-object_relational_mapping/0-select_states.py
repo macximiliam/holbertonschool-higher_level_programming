b2b7d084a6d62f0ba06fbe5a979a8e52908e0128
macxiliam <alexanderbiman@gmail.com>
2026-01-27 22:37:48 -0500

    Add a script that lists all states from the database hbtn_0e_0_usa

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,28 +1,31 @@
-#!/usr/bin/python3
-# This script connects to a MySQL database and lists all states from the database.
+#!/usr/bin/env python3
+# This script connects to a MySQL database and lists all states from hbtn_0e_0_usa.
 import MySQLdb
 import sys
 
 def list_states():
-    # Connects to the database using arguments and executes a query to fetch all states.
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
-    
-    cur = db.cursor()
-    # Executes the SQL command to retrieve states sorted by ID
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    # Connects to the database using command-line arguments and fetches all states.
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
         
-    cur.close()
-    db.close()
+        cur = db.cursor()
+        # Executes the query to get all states ordered by id.
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
+        
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+            
+        cur.close()
+        db.close()
+    except Exception:
+        pass
 
 if __name__ == "__main__":
     list_states()
