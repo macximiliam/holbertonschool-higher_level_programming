45916eda15495e9f424767659e7e238627b31373
macxiliam <alexanderbiman@gmail.com>
2026-01-27 22:41:56 -0500

    Add a script that lists all states from the database hbtn_0e_0_usa

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python3
-# This script connects to a MySQL database and lists all states from hbtn_0e_0_usa.
+"""
+List all states from the database hbtn_0e_0_usa
+"""
+
 import MySQLdb
 import sys
 
-def list_states():
-    # Connects to the database using command-line arguments and fetches all states.
-    try:
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3]
-        )
-        
-        cur = db.cursor()
-        # Executes the query to get all states ordered by id.
-        cur.execute("SELECT * FROM states ORDER BY id ASC")
-        
-        rows = cur.fetchall()
-        for row in rows:
-            print(row)
-            
-        cur.close()
-        db.close()
-    except Exception:
-        pass
-
 if __name__ == "__main__":
-    list_states()
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    db.close()
