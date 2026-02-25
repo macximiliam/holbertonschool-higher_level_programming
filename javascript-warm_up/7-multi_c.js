7bae0224ae83b58de6c7de2794aa3f179353071a
macximiliam <alexandebigman29r@gmail.com>
2026-02-24 20:10:39 -0500

    Add a project about js

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -3,11 +3,11 @@
 const times = parseInt(process.argv[2]);
 
 if (isNaN(times)) {
-    console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else if (times > 0) {
-    let i = 0;
-    while (i < times) {
-        console.log('C is fun');
+  let i = 0;
+  while (i < times) {
+    console.log('C is fun');
         i++;
-    }
+  }
 }
